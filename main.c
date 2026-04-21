ae618979513d80711e9bfe041a33b9df4ba3a5af
BrianVuni <bryanvuni@gmail.com>
2026-04-21 19:16:54 +0300

    Assignment two

--- main.c
+++ main.c
@@ -7,8 +7,28 @@ Registration Number: 25/U/BIO/01372/PD
 
 int main(void) {
     /* Declare variables here */
+    // these are the scores for sem1
+    int s1_1, s1_2, s1_3, s1_4, s1_5, s1_6, s1_7, s1_8;
+    // these are the scores for sem2
+    int s2_1, s2_2, s2_3, s2_4, s2_5, s2_6, s2_7, s2_8;
+
+    // Credit Units (CUs)
+    int c1_1=4, c1_2=3, c1_3=3, c1_4=3, c1_5=3, c1_6=3, c1_7=2, c1_8=3;
+    int c2_1=4, c2_2=3, c2_3=3, c2_4=3, c2_5=3, c2_6=3, c2_7=3, c2_8=3;
+
+    // Grade points
+    double gp1_1, gp1_2, gp1_3, gp1_4, gp1_5, gp1_6, gp1_7, gp1_8;
+    double gp2_1, gp2_2, gp2_3, gp2_4, gp2_5, gp2_6, gp2_7, gp2_8;
+
+    // Grades f
+    char g1_1, g1_2, g1_3, g1_4, g1_5, g1_6, g1_7, g1_8;
+    char g2_1, g2_2, g2_3, g2_4, g2_5, g2_6, g2_7, g2_8;
 
     /* Read 16 scores */
+   
+    scanf("%d %d %d %d %d %d %d %d", &s1_1, &s1_2, &s1_3, &s1_4, &s1_5, &s1_6, &s1_7, &s1_8);
+   
+    scanf("%d %d %d %d %d %d %d %d", &s2_1, &s2_2, &s2_3, &s2_4, &s2_5, &s2_6, &s2_7, &s2_8);
 
     /* Validate input */
 
